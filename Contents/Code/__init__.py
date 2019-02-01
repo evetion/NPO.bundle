a2f6bd9538a3a495f019b085595527813a1ba90d
Maarten Pronk <git@evetion.nl>
2019-02-02 00:10:08 +0100

    Added first working version.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,199 +1,297 @@
 TITLE = 'NPO'
-API_BASE_URL = 'https://apps-api.uitzendinggemist.nl'
+API_BASE_URL = 'https://start-api.npo.nl/'
 EPISODE_URL = 'https://www.npo.nl/redirect/00-00-0000/%s'
 DAY = ['Maandag', 'Dinsdag', 'Woensdag', 'Donderdag', 'Vrijdag', 'Zaterdag', 'Zondag']
-MONTH = ['', 'januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'oktober', 'november', 'december']
+MONTH = ['', 'januari', 'februari', 'maart', 'april', 'mei', 'juni',
+         'juli', 'augustus', 'september', 'oktober', 'november', 'december']
 
 ####################################################################################################
+
 def Start():
 
-	ObjectContainer.title1 = TITLE
-	HTTP.CacheTime = 300
-	HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36'
+    ObjectContainer.title1 = TITLE
+    HTTP.CacheTime = 300
+    HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36'
+    HTTP.Headers['ApiKey'] = 'e45fe473feaf42ad9a215007c6aa5e7e'
 
 ####################################################################################################
+
+# API
+# Website
+# https://start-api.npo.nl/page/catalogue?ApiKey=e45fe473feaf42ad9a215007c6aa5e7e
+# https://start-api.npo.nl/page/franchise/KN_1676727?ApiKey=e45fe473feaf42ad9a215007c6aa5e7e
+# https://start-api.npo.nl/page/episode/POW_03991657?ApiKey=e45fe473feaf42ad9a215007c6aa5e7e
+
+# Backend
+# https://start-api.npo.nl/media/series/AT_2046031/seasons/?ApiKey=e45fe473feaf42ad9a215007c6aa5e7e
+# https://start-api.npo.nl/media/POW_03991657?ApiKey=e45fe473feaf42ad9a215007c6aa5e7e
+# https://start-api.npo.nl/media/POW_03991657/stream/?ApiKey=e45fe473feaf42ad9a215007c6aa5e7e
+
+# payload for stream "{\"profile\":\"hls\",\"viewer\":1061049068313,\"options\":{\"startOver\":false}}"
+# player token => http://ida.omroep.nl/app.php/auth
+
 @handler('/video/npo', TITLE)
 def MainMenu(**kwargs):
 
-	oc = ObjectContainer()
+    oc = ObjectContainer()
 
 #	oc.add(DirectoryObject(key=Callback(Overview, title='Kijktips', path='tips.json'), title='Kijktips'))
-	oc.add(DirectoryObject(key=Callback(Overview, title='Populair', path='episodes/popular.json'), title='Populair'))
-	oc.add(DirectoryObject(key=Callback(Overview, title='Meest bekeken', path='episodes/trending.json'), title='Meest bekeken'))
-	oc.add(DirectoryObject(key=Callback(OnDemand), title='Gemist'))
-	oc.add(DirectoryObject(key=Callback(AZ), title='Programma\'s A-Z'))
+    # oc.add(DirectoryObject(key=Callback(Overview, title='Populair', path='episodes/popular.json'), title='Populair'))
+    oc.add(DirectoryObject(key=Callback(Series, series_id="POW_03108599"), title='HHB'))
+    oc.add(DirectoryObject(key=Callback(OnDemand), title='Gemist'))
+    oc.add(DirectoryObject(key=Callback(AZ), title='Programma\'s A-Z'))
 
-	return oc
+    return oc
 
 ####################################################################################################
+
+
 @route('/video/npo/overview')
 def Overview(title, path, **kwargs):
 
-	oc = ObjectContainer(title2=title)
-	json_obj = JSON.ObjectFromURL('%s/%s' % (API_BASE_URL, path))
-	episodes = []
+    oc = ObjectContainer(title2=title)
+    json_obj = JSON.ObjectFromURL('%s/%s' % (API_BASE_URL, path))
 
-	for video in json_obj:
+    # for programme in find_grid_all(json_obj, pages=3):
+    programmes = find_grid_all(json_obj, pages=7)
+    programmes = sorted(programmes, key=lambda x: x.get("broadcastDate", 0), reverse=True)
 
-		if 'episodes/' in path: # Populair en Meest bekeken
+    for programme in programmes:
 
-			title = '%s - %s' % (video['series']['name'], video['name']) if video['name'] != "" else video['series']['name']
-			summary = video['description']
-			episode_id = video['mid']
+        thumbs = []
+        try:
+            thumbs.append(programme['images']['original']['formats']['original']['source'])
+        except:
+            pass
 
-			thumbs = []
-			if video['image']: thumbs.append(video['image'])
-			if 'stills' in video and video['stills']: thumbs.append(video['stills'][0]['url'])
-			if video['series']['image']: thumbs.append(video['series']['image'])
+        if programme.get("isMovie"):
 
-			broadcasted_at = video['broadcasted_at']
+            try:
+                oc.add(VideoClipObject(
+                    url=EPISODE_URL % (programme['id']),
+                    title=programme['title'],
+                    summary=programme['description'],
+                    duration=programme['duration'] * 1000,
+                    # originally_available_at=airdate.date(),
+                    thumb=Resource.ContentsOfURLWithFallback(thumbs),
+                ))
+            except:
+                pass
 
-		elif 'broadcasts/' in path: # Gemist
+        else:
+            oc.add(DirectoryObject(
+                key=Callback(Series, series_id=programme['id']),
+                title=programme['title'],
+                summary=programme['description'],
+                thumb=Resource.ContentsOfURLWithFallback(thumbs)
+            ))
 
-			title = '%s - %s' % (video['episode']['series']['name'], video['episode']['name']) if video['episode']['series']['name'].lower() not in video['episode']['name'].lower() else video['episode']['name']
-			title = title.strip(' -')
-			summary = video['episode']['description']
-			episode_id = video['episode']['mid']
-
-			thumbs = []
-			if video['episode']['image']: thumbs.append(video['episode']['image'])
-			if 'stills' in video['episode'] and video['episode']['stills']: thumbs.append(video['episode']['stills'][0]['url'])
-			if video['episode']['series']['image']: thumbs.append(video['episode']['series']['image'])
-
-			broadcasted_at = video['episode']['broadcasted_at']
-
-		else: # Kijktips
-
-			title = '%s - %s' % (video['episode']['series']['name'], video['name']) if video['episode']['series']['name'].lower() not in video['name'].lower() else video['name']
-			summary = video['description']
-			episode_id = video['episode']['mid']
-
-			thumbs = []
-			if video['episode']['image']: thumbs.append(video['episode']['image'])
-			if 'stills' in video['episode'] and video['episode']['stills']: thumbs.append(video['episode']['stills'][0]['url'])
-			if video['episode']['series']['image']: thumbs.append(video['episode']['series']['image'])
-
-			broadcasted_at = video['episode']['broadcasted_at']
-
-		episodes.append({
-			'episode_id': episode_id,
-			'title': title,
-			'summary': summary,
-			'thumbs': thumbs,
-			'broadcasted_at': broadcasted_at
-		})
-
-	episodes = sorted(episodes, key=lambda k: k['broadcasted_at'], reverse=True)
-
-	for episode in episodes:
-
-		oc.add(DirectoryObject(
-			key = Callback(Episode, episode_id=episode['episode_id']),
-			title = episode['title'],
-			summary = episode['summary'],
-			thumb = Resource.ContentsOfURLWithFallback(episode['thumbs'])
-		))
-
-	return oc
+    return oc
 
 ####################################################################################################
-@route('/video/npo/episode/{episode_id}')
-def Episode(episode_id, **kwargs):
 
-	video = JSON.ObjectFromURL('%s/episodes/%s.json' % (API_BASE_URL, episode_id))
 
-	oc = ObjectContainer(title2=video['series']['name'])
-
-	airdate = Datetime.FromTimestamp(video['broadcasted_at'])
-	title = video['name'] if video['name'] else video['series']['name']
-	title = '%s (%s %s %s)' % (title, airdate.day, MONTH[airdate.month], airdate.year)
-
-	thumbs = []
-	if video['image']: thumbs.append(video['image'])
-	if 'stills' in video and video['stills']: thumbs.append(video['stills'][0]['url'])
-	if video['series']['image']: thumbs.append(video['series']['image'])
-
-	oc.add(VideoClipObject(
-		url = EPISODE_URL % (video['mid']),
-		title = title,
-		summary = video['description'],
-		duration = video['duration'] * 1000,
-		originally_available_at = airdate.date(),
-		thumb = Resource.ContentsOfURLWithFallback(thumbs)
-	))
-
-	oc.add(DirectoryObject(
-		key = Callback(Series, series_id=video['series']['id']),
-		title = 'Alle afleveringen'
-	))
-
-	return oc
+# @route('/video/npo/episode/{episode_id}')
+# def Episode(episode_id, **kwargs):
+#
+#     video = JSON.ObjectFromURL('%s/page/episode/%s' % (API_BASE_URL, episode_id))
+#
+#     oc = ObjectContainer(title2=video['series']['name'])
+#
+#     airdate = Datetime.FromTimestamp(video['broadcasted_at'])
+#     title = video['name'] if video['name'] else video['series']['name']
+#     title = '%s (%s %s %s)' % (title, airdate.day, MONTH[airdate.month], airdate.year)
+#
+#     thumbs = []
+#     if video['image']:
+#         thumbs.append(video['image'])
+#     if 'stills' in video and video['stills']:
+#         thumbs.append(video['stills'][0]['url'])
+#     if video['series']['image']:
+#         thumbs.append(video['series']['image'])
+#
+#     oc.add(VideoClipObject(
+#         url=EPISODE_URL % (video['mid']),
+#         title=title,
+#         summary=video['description'],
+#         duration=video['duration'] * 1000,
+#         originally_available_at=airdate.date(),
+#         thumb=Resource.ContentsOfURLWithFallback(thumbs)
+#     ))
+#
+#     oc.add(DirectoryObject(
+#         key=Callback(Series, series_id=video['series']['id']),
+#         title='Alle afleveringen'
+#     ))
+#
+#     return oc
 
 ####################################################################################################
+
+def find_grid_all(obj, pages=2):
+    all = find_grid(obj)
+    Log("Got first page")
+    for component in obj["components"]:
+        if component.get("type") == "grid":
+            data = component.get("data")
+            if "_links" in data and "next" in data["_links"]:
+                i = 0
+                while True:
+                    if i >= pages: break
+                    i += 1
+                    next = data["_links"].get("next")
+                    Log("Getting next page: {}".format(next))
+                    if next is not None:
+                        data = JSON.ObjectFromURL(next["href"])
+                        items = data.get("items")
+                        if items is not None:
+                            all.extend(items)
+                        else:
+                            break
+                    else:
+                        break
+    return all
+
+
+def find_grid(obj):
+    for component in obj["components"]:
+        if component.get("type") == "grid":
+            data = component.get("data")
+            items = data.get("items")
+            return items
+
+
+def find_grid_all2(obj, pages=2):
+    all = obj.get("items")
+    Log("Got first page")
+    if "_links" in obj and "next" in obj["_links"]:
+        i = 0
+        while True:
+            if i >= pages: break
+            i += 1
+            next = obj["_links"].get("next")
+            Log("Getting next page: {}".format(next))
+            if next is not None:
+                obj = JSON.ObjectFromURL(next["href"])
+                items = obj.get("items")
+                if items is not None:
+                    all.extend(items)
+                else:
+                    break
+            else:
+                break
+    return all
+
+
 @route('/video/npo/series/{series_id}')
 def Series(series_id, **kwargs):
 
-	json_obj = JSON.ObjectFromURL('%s/series/%s.json' % (API_BASE_URL, series_id))
+    # url = '%s/page/franchise/%s' % (API_BASE_URL, series_id)
+    url = '%s/media/series/%s/episodes' % (API_BASE_URL, series_id)
+    json_obj = JSON.ObjectFromURL(url)
+    # print(json_obj)
+    episodes = find_grid_all2(json_obj, pages=5)
+    # Log(episodes)
 
-	oc = ObjectContainer(title2=json_obj['name'])
+    if len(episodes) > 0:
+        oc = ObjectContainer(title2=episodes[0]['title'])
+    else:
+        return ObjectContainer(title2="No episodes")
 
-	for video in json_obj['episodes']:
+    episodes = sorted(episodes, key=lambda x: x["broadcastDate"], reverse=True)
 
-		airdate = Datetime.FromTimestamp(video['broadcasted_at'])
-		title = video['name'] if video['name'] else json_obj['name']
-		title = '%s (%s %s %s)' % (title, airdate.day, MONTH[airdate.month], airdate.year)
+    for video in episodes:
 
-		thumbs = []
-		if video['image']: thumbs.append(video['image'])
-		if 'stills' in video and video['stills']: thumbs.append(video['stills'][0]['url'])
-		if json_obj['image']: thumbs.append(json_obj['image'])
+        # 2019-01-20T19:25:00Z
+        # airdate = Datetime.strptime(video['broadcastDate'], '%Y-%M-%DT%XZ')
+        title = video['episodeTitle']
+        # title = '%s (%s %s %s)' % (title, airdate.day, MONTH[airdate.month], airdate.year)
 
-		oc.add(VideoClipObject(
-			url = EPISODE_URL % (video['mid']),
-			title = title,
-			summary = video['description'],
-			duration = video['duration'] * 1000,
-			originally_available_at = airdate.date(),
-			thumb = Resource.ContentsOfURLWithFallback(thumbs)
-		))
+        thumbs = []
+        try:
+            thumbs.append(programme['images']['original']['formats']['original']['source'])
+        except:
+            pass
 
-	return oc
+        # if video['image']:
+        #     thumbs.append(video['image'])
+        # if 'stills' in video and video['stills']:
+        #     thumbs.append(video['stills'][0]['url'])
+        # if json_obj['image']:
+        #     thumbs.append(json_obj['image'])
+        try:
+            oc.add(VideoClipObject(
+                url=EPISODE_URL % (video['id']),
+                title=title,
+                summary=video['description'],
+                duration=video['duration'] * 1000,
+                # originally_available_at=airdate.date(),
+                thumb=Resource.ContentsOfURLWithFallback(thumbs)
+            ))
+        except:
+            Log("{} crashed".format(video['id']))
+
+    return oc
 
 ####################################################################################################
+
+
 @route('/video/npo/ondemand')
 def OnDemand(**kwargs):
 
-	oc = ObjectContainer(title2='Gemist')
-	delta = Datetime.Delta(days=1)
-	yesterday = (Datetime.Now() - delta).date()
+    oc = ObjectContainer(title2='Gemist')
+    delta = Datetime.Delta(days=1)
+    yesterday = (Datetime.Now() - delta).date()
 
-	oc.add(DirectoryObject(key=Callback(Overview, title='Laatst toegevoegd', path='broadcasts/recent.json'), title='Laatst toegevoegd'))
-	oc.add(DirectoryObject(key=Callback(Overview, title='Gisteren', path='broadcasts/%s.json' % (yesterday)), title='Gisteren'))
+    for i in range(0, 10):
 
-	for i in range(2, 10):
+        date_object = Datetime.Now() - (delta * i)
+        title = '%s %s %s' % (DAY[date_object.weekday()], date_object.day, MONTH[date_object.month])
 
-		date_object = Datetime.Now() - (delta * i)
-		title = '%s %s %s' % (DAY[date_object.weekday()], date_object.day, MONTH[date_object.month])
+        oc.add(DirectoryObject(key=Callback(Overview, title=title, path='/page/catalogue/?dateFrom=%s' % (date_object.date())), title=title))
 
-		oc.add(DirectoryObject(key=Callback(Overview, title=title, path='broadcasts/%s.json' % (date_object.date())), title=title))
-
-	return oc
+    return oc
 
 ####################################################################################################
+
+
 @route('/video/npo/az')
 def AZ(**kwargs):
 
-	oc = ObjectContainer(title2='Programma\'s A-Z')
+    oc = ObjectContainer(title2='Programma\'s A-Z')
 
-	json_obj = JSON.ObjectFromURL('%s/series.json' % (API_BASE_URL))
+    json_obj = JSON.ObjectFromURL('%s/page/catalogue' % (API_BASE_URL))
 
-	for programme in json_obj:
+    programmes = find_grid_all(json_obj, pages=7)
+    programmes = sorted(programmes, key=lambda x: x["title"], reverse=True)
 
-		oc.add(DirectoryObject(
-			key = Callback(Series, series_id=programme['mid']),
-			title = programme['name'],
-			summary = programme['description'],
-			thumb = Resource.ContentsOfURLWithFallback(programme['image'])
-		))
+    for programme in programmes:
 
-	return oc
+        thumbs = []
+        try:
+            thumbs.append(programme['images']['original']['formats']['original']['source'])
+        except:
+            pass
+
+        if programme.get("isMovie"):
+            try:
+                oc.add(VideoClipObject(
+                    url=EPISODE_URL % (programme['id']),
+                    title=programme['title'],
+                    summary=programme['description'],
+                    duration=programme['duration'] * 1000,
+                    # originally_available_at=airdate.date(),
+                    thumb=Resource.ContentsOfURLWithFallback(thumbs)
+                ))
+            except:
+                pass
+
+        else:
+            oc.add(DirectoryObject(
+                key=Callback(Series, series_id=programme['id']),
+                title=programme['title'],
+                summary=programme['description'],
+                thumb=Resource.ContentsOfURLWithFallback(thumbs)
+            ))
+
+    return oc
